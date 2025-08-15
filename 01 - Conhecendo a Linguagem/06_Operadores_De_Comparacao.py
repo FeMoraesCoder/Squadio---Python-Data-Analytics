4f88584a47e00e10a93996530fcc5469f3098010
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-08-15 15:39:28 -0300

    Aulas iniciais: Tipos de Operadores

---
+++ 01 - Conhecendo a Linguagem/06_Operadores_De_Comparacao.py
@@ -0,0 +1,25 @@
+'''
+Igualdade: ==
+
+Diferença: !=
+
+Maior: >
+Maior igual: >=
+
+Menor: <
+menor igual: <=
+
+'''
+#%%
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
+
+#%%
+
---
+++ 01 - Conhecendo a Linguagem/07_Operadores_Atribuicao.py
@@ -0,0 +1,48 @@
+'''
+Operadores de Atribuição
+
+São operadores utilizados para definir o valor inicial ou
+sobrescrever o valor de uma variável.
+
+Atribuição simples: =
+    saldo = 500
+
+Atribuição com adição: +=
+    saldo += 500
+
+Atribuição com subtração: -=
+    saldo -= 500
+
+outros seguem a mesma lógica: 
+    saldo /= 100
+    saldo //= 100
+    saldo **= 100
+    saldo *= 100
+
+    
+'''
+#%% 
+saldo = 500
+
+print(saldo)
+
+saldo += 500
+print(saldo)
+
+saldo -= 500
+print(saldo)
+
+saldo /= 100
+print(saldo)
+
+saldo //= 100
+print(saldo)
+
+saldo **= 100
+print(saldo)
+
+saldo *= 100
+print(saldo)
+
+
+# %%
