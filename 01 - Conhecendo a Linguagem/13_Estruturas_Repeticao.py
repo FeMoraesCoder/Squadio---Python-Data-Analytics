f3fd0bdef03f00f6968a6245b6fd34f4e7887fae
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-08-20 17:16:27 -0300

    Atualiação

--- 01 - Conhecendo a Linguagem/13_Estruturas_Repeticao.py
+++ 01 - Conhecendo a Linguagem/13_Estruturas_Repeticao.py
@@ -45,6 +45,39 @@ step (opcional)
 
 #%%
 
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
 
 
 #%%
+
+'''
+Comando While
+
+O comando while é usado para repetir um bloco de código
+várias vezes. Faz sentido usar while quando não sabemos o
+número exato de vezes que nosso bloco de código deve ser
+executado.
+
+'''
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
+
+    if opcao == 1:
+        print('Sacando...')
+    elif opcao == 2:
+        print("Exibindo o extrato...")
+    
+# %%
+
+while True:
+    numero = int(input("Informe um número: "))
+
+    if numero == 10:
+        break
+
+    print(numero)
+
+# %%
