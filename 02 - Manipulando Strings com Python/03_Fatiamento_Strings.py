a5cbd8cbc4ab8dee0565847bb4b01d9b7668d8d4
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-08-22 13:45:01 -0300

    Módulo 2 - Manipulando Strings

---
+++ 02 - Manipulando Strings com Python/03_Fatiamento_Strings.py
@@ -0,0 +1,19 @@
+'''
+Fatiamento de strings é uma técnica utilizada para retornar
+substrings (partes da string original), informando inicio (start),
+fim (stop) e passo (step): 
+
+[start: stop[,step]].
+
+'''
+#%%
+nome = "Felipe de Moraes Aparecido"
+
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:20:3])
+print(nome[:])
+print(nome[::-1])
+#%%
---
+++ 02 - Manipulando Strings com Python/04_Strings_Multiplas_Linhas.py
@@ -0,0 +1,35 @@
+"""
+Strings de múltiplas linhas são definidas informando 3 aspas
+simples ou duplas durante a atribuição. Elas podem ocupar
+várias linhas do código, e todos os espaços em branco são
+incluídos na string final. 
+
+"""
+# Strings Triplas ou de Multiplas linhas
+#%%
+nome = "Felipe"
+
+mensagem = f'''
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+    '''
+
+print(mensagem)
+# %%
+
+print(
+    """
+    ========== MENU ==========
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ==========================
+
+        Obrigado por usar nosso sistema !!!
+"""
+)
+
+# %%
